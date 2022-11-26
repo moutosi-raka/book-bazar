762e195e59c4c90bdd75fc144ecc76d424428e86
moutosi-raka <raka29077997@gmail.com>
2022-11-26 09:27:33 +0600

    create category api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port= process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +8,32 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+require('dotenv').config();
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dtu3pyw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+       
+        const bookCategoriesCollection = client.db('bookBazar').collection('allCategories');
+
+        app.get('/category/:id', async(req, res)=>{
+            const id = parseInt(req.params.id);
+            const query = {category_id: id};
+            const cursor = await bookCategoriesCollection.find(query).toArray();
+            res.send(cursor)
+        })
+    }
+    finally{
+
+    }
+
+}
+run().catch(err => console.log(err))
+
 
 app.get('/', async(req,res)=>{
     res.send('book bazar server is running.....')
